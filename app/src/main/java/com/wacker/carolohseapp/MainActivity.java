fca0c9e14822eed481ead55ed0af2090c5a0bc1e
Kowait00 <kowait00@hs-esslingen.de>
2016-12-11 17:14:18 +0100

    -Added app info screen with copyright information for graphics etc.

--- app/src/main/java/com/wacker/carolohseapp/MainActivity.java
+++ app/src/main/java/com/wacker/carolohseapp/MainActivity.java
@@ -102,7 +102,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_appInfo) {
+            Intent intent = new Intent(this, AppInfoActivity.class);
+            startActivity(intent);
             return true;
         }
 
